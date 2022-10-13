75557689e9234c46b0b9b3a7ec0662d07da40051
Mario <m.farfan285@gmail.com>
2022-10-13 14:00:00 -0500

    Agregar equipos
    Front y back para agregar equipos a la base de datos, ya se reflejan los cambios en la BD

--- src/rutas/links.js
+++ src/rutas/links.js
@@ -3,12 +3,30 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/login', (req, res) => {
+    res.render('inventarios/login')
+});
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.send('Logeando');
+});
+
 router.get('/inventarios/agregar_dispositivo', (req, res) => {
     res.render('inventarios/agregar')
 });
 
-router.post('/inventarios', (req, res) => {
-    console.log(req.body);
+router.post('/inventarios/agregar_dispositivo', async(req, res) => {
+    const{ folioinv, noserie, marca, tipo, foliocpu, idarea } = req.body;
+    const newDispositivo = {
+        folioinv, 
+        noserie, 
+        marca, 
+        tipo, 
+        foliocpu, 
+        idarea
+    };
+    await pool.query('INSERT INTO EQUIPO set ?', [newDispositivo]);
+    console.log(newDispositivo);
     res.send('recived')
 });
 module.exports = router;
