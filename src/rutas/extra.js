e3e6c655b6b69fcc18f0a99becfeab383f05d9ca
keysiul <key.siul@gmail.com>
2023-01-17 22:53:45 -0600

    Editar y eliminar software

--- src/rutas/extra.js
+++ src/rutas/extra.js
@@ -22,4 +22,34 @@ router.get('/listar_software', isLoggedIn, async (req, res) => {
     res.render('extras/listar_software', {soft});
 });
 
+//Eliminar registros
+router.get('/listar_software/eliminar/:id', isLoggedIn,  async (req, res) => {
+    const { conexion } = require('../lib/passport');
+    const {id} = req.params;
+    await conexion.query('DELETE FROM SOFTWARE WHERE ID_SOFTWARE = (?)', [id]);
+    req.flash('mensaje', 'Software eliminado con exito');
+    const soft = await conexion.query('SELECT * FROM SOFTWARE');
+    res.render('extras/listar_software', {soft});
+});
+
+//Editar registros 
+router.get('/listar_software/editar/:id', isLoggedIn,  async(req, res) => {
+    const {id} = req.params;
+    const { conexion } = require('../lib/passport');
+    const equipo = await conexion.query('SELECT * FROM SOFTWARE WHERE ID_SOFTWARE = ?', [id]);
+    res.render('extras/editar_software', {equipo: equipo[0]});
+});
+
+router.post('/listar_software/editar/:id', isLoggedIn,  async(req, res) => {
+    const {id} = req.params;
+    const { conexion } = require('../lib/passport');
+    const{ software, tipolicencia, licencia } = req.body;
+    const newInsert = { software, tipolicencia, licencia}; 
+
+    await  conexion.query('UPDATE SOFTWARE SET ? WHERE ID_SOFTWARE = ?', [newInsert, id]);
+    req.flash('mensaje', 'Software editado con exito');
+    const soft = await conexion.query('SELECT * FROM SOFTWARE');
+    res.render('extras/listar_software', {soft});
+});
+
 module.exports = router;
